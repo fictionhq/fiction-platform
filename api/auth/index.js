c04d12ce742f65beeb44e820f00f96dee982b728
rajikaimal <inbox.rajika@gmail.com>
2018-12-21 22:55:59 +0530

    Update pkg.json

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const authRoutes = require('./routes/auth');
+const authRoutes = require("./routes/auth");
 const mongoose = require("./mongoose");
 const config = require("config");
+const expressValidator = require("express-validator");
 const PORT = process.env.AUTH_PORT || 3001;
 
 // connect to mongoose
@@ -13,12 +14,12 @@ mongoose(username, password);
 
 // JSON body parser
 app.use(bodyParser.json());
+app.use(expressValidator());
 // register routes
 app.use("/api/v1/auth", authRoutes(mongoose));
 
-app.listen(PORT, (err) => {
-    if (err) 
-        throw new Error(err);
-    
-    console.log(`Auth service running at ${PORT}`);
+app.listen(PORT, err => {
+  if (err) throw new Error(err);
+
+  console.log(`Auth service running at ${PORT}`);
 });
